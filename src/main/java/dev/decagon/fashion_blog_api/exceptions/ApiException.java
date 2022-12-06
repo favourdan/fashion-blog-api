9c4f5be81cc4fd354ef97015f7284daf0cae89d8
favourdan <106870617+favourdan@users.noreply.github.com>
2022-12-06 17:59:54 +0100

    Refactored the ApiException.

--- src/main/java/dev/decagon/fashion_blog_api/exceptions/ApiException.java
+++ src/main/java/dev/decagon/fashion_blog_api/exceptions/ApiException.java
@@ -1,8 +1,19 @@
 package dev.decagon.fashion_blog_api.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-public record ApiException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiException {
+    private String message;
+    private HttpStatus httpStatus;
+    private ZonedDateTime zonedDateTime;
 }
